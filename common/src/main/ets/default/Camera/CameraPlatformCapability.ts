ec0dd92c0f15276bc9548c466c04377f8e0b5e87
jasper-zhao <zhaochong5@huawei.com>
2022-05-20 15:27:34 +0800

    Signed-off-by: jasper-zhao <zhaochong5@huawei.com>
    update new module code

---
+++ common/src/main/ets/default/Camera/CameraPlatformCapability.ts
@@ -0,0 +1,140 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { CameraId } from '../setting/CameraId'
+import { CLog } from '../Utils/CLog'
+
+export class CameraPlatformCapability {
+  private TAG: string = '[CameraPlatformCapability]:'
+  private static sInstance: CameraPlatformCapability = undefined
+  public mZoomRatioRangeMap = new Map()
+  public mPhotoPreviewSizeMap = new Map()
+  public mVideoPreviewSizeMap = new Map()
+  public mImageSizeMap = new Map()
+  public mVideoFrameSizeMap = new Map()
+  public mCameraCount = 0
+  // TODO 需要针对不同设备固定不同的值
+  public mPhotoPreviewSize = [
+    { width: 640, height: 480 }, //Photo 4:3
+    { width: 720, height: 720 }, //Photo 1:1
+    { width: 1920, height: 1080 }, //Photo 16:9
+  ]
+  public mVideoPreviewSize = [
+    { width: 1920, height: 1080 }, //Video 16:9 720p
+    { width: 1920, height: 1080 }, //Video 16:9 1080p
+    { width: 1920, height: 1080 } //Video 16:9 4k
+  ]
+  public mImageSize = [
+    { width: 1280, height: 960 }, //4:3
+    { width: 3120, height: 3120 }, //1:1
+    { width: 1920, height: 1080 } //16:9
+  ]
+  public mVideoFrameSize = [
+    { width: 1280, height: 720 }, //16:9 720p
+    { width: 1920, height: 1080 }, //16:9 1080p
+    { width: 3840, height: 2160 } //16:9 4k
+  ]
+
+  constructor() {
+  }
+
+  public static getInstance(): CameraPlatformCapability {
+    if (!CameraPlatformCapability.sInstance) {
+      CameraPlatformCapability.sInstance = new CameraPlatformCapability()
+    }
+    return CameraPlatformCapability.sInstance;
+  }
+
+  public async init() {
+    CLog.info(`${this.TAG} init E.`)
+  }
+
+  public async calcSupportedSizes(cameraInput) {
+    CLog.info(`${this.TAG} calcSupportedSizes called.`)
+    let photoSize = await cameraInput.getSupportedSizes(2000) //CAMERA_FORMAT_JPEG
+    if (!photoSize) {
+      return
+    }
+    let previewCurSize = await cameraInput.getSupportedSizes(1003) //CAMERA_FORMAT_YCRCb_420_SP
+    CLog.info(`${this.TAG} calcSupportedSizes photoSize ${JSON.stringify(photoSize)}`)
+    CLog.info(`${this.TAG} calcSupportedSizes previewCurSize ${JSON.stringify(previewCurSize)}`)
+    this.mImageSize[0] = this.getMaxSize(photoSize, 4, 3)
+    this.mImageSize[1] = this.getMaxSize(photoSize, 1, 1)
+    this.mImageSize[2] = this.getMaxSize(photoSize, 16, 9)
+    this.mPhotoPreviewSize[0] = this.getMaxSize(previewCurSize, 4, 3)
+    this.mPhotoPreviewSize[1] = this.getMaxSize(previewCurSize, 1, 1)
+    this.mPhotoPreviewSize[2] = this.getMaxSize(previewCurSize, 16, 9)
+
+    this.mVideoFrameSize[0] = this.getSpecifiedSize(previewCurSize, 1280, 720)
+    this.mVideoFrameSize[1] = this.getSpecifiedSize(previewCurSize, 1920, 1080)
+    this.mVideoFrameSize[2] = this.getSpecifiedSize(previewCurSize, 3840, 2160)
+    this.mVideoPreviewSize[0] = this.mVideoFrameSize[0]
+    this.mVideoPreviewSize[1] = this.mVideoFrameSize[1]
+    this.mVideoPreviewSize[2] = this.mVideoFrameSize[2]
+  }
+
+  private getMaxSize(sizeList, width, height) {
+    let maxSize = { width: 0, height: 0 }
+    let fitList = []
+    for (let i = 0; i < sizeList.length; i++) {
+      //      console.info('--getMaxSize size list width: ' + sizeList[i].width + ' height: ' + sizeList[i].height)
+      let errorValue = sizeList[i].width * height - sizeList[i].height * width
+      if (errorValue <= 16 && errorValue >= -16) {
+        fitList.push(sizeList[i])
+      }
+    }
+    console.info('getMaxSize fitList.length: ' + fitList.length)
+    if (fitList.length == 0) {
+      maxSize.width = 640
+      maxSize.height = 480
+      console.info('getMaxSize.width: ' + maxSize.width + ' height: ' + maxSize.height)
+      return maxSize
+    } else {
+      let index = Math.floor(fitList.length / 2)
+      maxSize.width = fitList[index].width
+      maxSize.height = fitList[index].height
+      console.info('getMaxSize.width: ' + maxSize.width + ' height: ' + maxSize.height)
+    }
+    return maxSize
+  }
+
+  private getSpecifiedSize(sizeList, width, height) {
+    let specifiedSize = { width: 0, height: 0 }
+    for (let i = 0; i < sizeList.length; i++) {
+      //      console.info('--Preview size list width: ' + sizeList[i].width + ' height: ' + sizeList[i].height)
+      let widthError = sizeList[i].width - width
+      let heightError = sizeList[i].height - height
+      if (widthError <= 4 && widthError >= -4 && heightError <= 4 && heightError >= -4) {
+        if (sizeList[i].width > specifiedSize.width) {
+          specifiedSize.width = sizeList[i].width
+          specifiedSize.height = sizeList[i].height
+        }
+      }
+    }
+    if (specifiedSize.width == 0) {
+      specifiedSize.width = 1920
+      specifiedSize.height = 1080
+    }
+    console.info('getSpecifiedSize.width: ' + specifiedSize.width + ' height: ' + specifiedSize.height)
+    return specifiedSize
+  }
+
+  public async getZoomRatioRange(cameraInput) {
+    CLog.info(`${this.TAG} getZoomRatioRange called`)
+    let zoomRatioRange = await cameraInput.getZoomRatioRange()
+    CLog.info(`${this.TAG} zoomRatioRange= ${zoomRatioRange}`)
+    return zoomRatioRange
+  }
+}
---
+++ common/src/main/ets/default/Utils/CameraWorker.ts
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { CLog } from '../Utils/CLog'
+import { Constants } from './Constants'
+import { EventBus } from './EventBus'
+import EventBusManager from './EventBusManager'
+import { WorkerManager } from './WorkerManager'
+import { FeatureManager
+} from '../../../../../../features/featurecommon/src/main/ets/com/ohos/featurecommon/featureservice/FeatureManager'
+
+export class CameraWorker {
+  private static TAG: string = '[CameraWorker]:'
+  private appEventBus: EventBus = EventBusManager.getWorkerInstance().getEventBus()
+  private workerManager: WorkerManager
+
+  constructor() {
+    this.workerManager = new WorkerManager()
+    if (globalThis.cameraFormParam != undefined) {
+      var featureManager = new FeatureManager(globalThis.cameraFormParam.mode)
+    } else {
+      var featureManager = new FeatureManager('PHOTO')
+    }
+    // 接收UI线程的消息，并继续发送
+    this.appEventBus.on('MAIN_TO_WORKER', (msg) => {
+      console.info(`[CameraWorker]:  action from main thread: ${JSON.stringify(msg)}`)
+      this.workerManager.onMessage(msg)
+    })
+  }
+
+  public static getInstance(): CameraWorker {
+    if (!AppStorage.Has(Constants.APP_KEY_CAMERA_WORKER)) {
+      AppStorage.SetOrCreate(Constants.APP_KEY_CAMERA_WORKER, new CameraWorker())
+      CLog.info(`${this.TAG} build new CameraWorker.`)
+    }
+    return AppStorage.Get(Constants.APP_KEY_CAMERA_WORKER);
+  }
+}
