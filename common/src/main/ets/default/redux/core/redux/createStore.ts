46955014209ea9292ce59a086d4979148941b244
lijinfengde123 <lijinfeng26@huawei.com>
2023-06-20 14:12:11 +0800

    失效字段更改、import顺序调整
    Signed-off-by: lijinfengde123 <lijinfeng26@huawei.com>

--- common/src/main/ets/default/redux/core/redux/createStore.ts
+++ common/src/main/ets/default/redux/core/redux/createStore.ts
@@ -1,6 +1,6 @@
 import $$observable from './utils/symbol-observable';
 
-import { Dispatch, ExtendState, Observer, PreloadedState, Store, StoreEnhancer } from './types/store';
+import type { Dispatch, ExtendState, Observer, PreloadedState, Store, StoreEnhancer } from './types/store';
 import type { Action } from './types/actions';
 import type { Reducer } from './types/reducers';
 import ActionTypes from './utils/actionTypes';
