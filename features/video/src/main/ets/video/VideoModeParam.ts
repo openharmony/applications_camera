203b30013eb1c038b6bdabcaf43f219dab144c27
lijinfengde123 <lijinfeng26@huawei.com>
2023-08-30 17:25:40 +0800

    来回切换模式服务卡片function未加载导致拍照录像失败问题
    Signed-off-by: lijinfengde123 <lijinfeng26@huawei.com>

--- features/video/src/main/ets/video/VideoModeParam.ts
+++ features/video/src/main/ets/video/VideoModeParam.ts
@@ -17,5 +17,5 @@ import { FunctionId } from '@ohos/common/src/main/ets/default/featureservice/Fun
 
 export class VideoModeParam {
   public readonly tabBar: string[] = ['', '', 'zoom', 'timer', 'setup'];
-  public readonly functions: FunctionId[] = [FunctionId.RECORDING_FUNCTION, FunctionId.ZOOM_FUNCTION];
+  public readonly functions: FunctionId[] = [FunctionId.RECORDING_FUNCTION, FunctionId.ZOOM_FUNCTION, FunctionId.CAPTURE_FUNCTION];
 }
