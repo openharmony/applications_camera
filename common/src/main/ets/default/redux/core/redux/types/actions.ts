31e0632753a990433d4d1507cda4006652a830ed
lijinfengde123 <lijinfeng26@huawei.com>
2024-01-02 10:56:23 +0800

    版权描述更改
    Signed-off-by: lijinfengde123 <lijinfeng26@huawei.com>

--- common/src/main/ets/default/redux/core/redux/types/actions.ts
+++ common/src/main/ets/default/redux/core/redux/types/actions.ts
@@ -1,20 +1,18 @@
-/**
- * An *action* is a plain object that represents an intention to change the
- * state. Actions are the only way to get data into the store. Any data,
- * whether from UI events, network callbacks, or other sources such as
- * WebSockets needs to eventually be dispatched as actions.
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Actions must have a `type` field that indicates the type of action being
- * performed. Types can be defined as constants and imported from another
- * module. It's better to use strings for `type` than Symbols because strings
- * are serializable.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Other than `type`, the structure of an action object is really up to you.
- * If you're interested, check out Flux Standard Action for recommendations on
- * how actions should be constructed.
- *
- * @template T the type of the action's `type` tag.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 export interface Action<T = any> {
   type: T
 }
